7d1d4b6f7b013da7fa13e91f773d4c227dbaeaa3
mail2vinowin <mail2vinowin@gmail.com>
2015-02-06 13:40:52 +0530

    cors issue and livereload

--- config/express.js
+++ config/express.js
@@ -149,7 +149,7 @@ module.exports = function(db) {
 		var certificate = fs.readFileSync('./config/sslcerts/cert.pem', 'utf8');
 
 		// Create HTTPS Server
-		var httpsServer = http.createServer({
+		var httpsServer = https.createServer({
 			key: privateKey,
 			cert: certificate
 		}, app);
